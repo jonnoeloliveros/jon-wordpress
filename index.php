462980c820dd1ab32f4d707faca3668343fe9bb2
Jon Noel Oliveros <47849219+jonnoeloliveros@users.noreply.github.com>
2024-10-04 10:19:10 +0800

    created the homepage template so that there will be a default template for pages

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
-    get_header();
-    if ( have_posts() ) {
-        while ( have_posts() ) {
-            the_post();
-            get_template_part('template-parts/banner');
-            get_template_part('template-parts/property', 'search');
-            get_template_part('template-parts/property', 'categories');
-            get_template_part('template-parts/about');
-            get_template_part('template-parts/property', 'list');
-            get_template_part('template-parts/call-to-action');
-            get_template_part('template-parts/team');
-            get_template_part('template-parts/testimonials');
-        }
-    } else {
-        echo '<p>No content found</p>';
+get_header();
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        ?>
+        <div class="container-xxl py-5">
+            <div class="container"> 
+                <?php the_content(); ?>
+            </div>
+        </div>
+        <?php
     }
-    get_footer();
+}
+get_footer();
 ?>
---
+++ page-templates/homepage.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Homepage
+ */
+get_header();
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        get_template_part('template-parts/banner');
+        get_template_part('template-parts/property', 'search');
+        get_template_part('template-parts/property', 'categories');
+        get_template_part('template-parts/about');
+        get_template_part('template-parts/property', 'list');
+        get_template_part('template-parts/call-to-action');
+        get_template_part('template-parts/team');
+        get_template_part('template-parts/testimonials');
+    }
+} else {
+    echo '<p>No content found</p>';
+}
+get_footer();
+?>
